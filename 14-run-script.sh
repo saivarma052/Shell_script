fe75f94793fe53bb3734eef966d759392f7e94b2
saivarma052 <saivarmadanduprlou052@gmail.com>
2024-12-09 14:02:31 +0530

    script for files

---
+++ 14-run-script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir test1 test2
+
+cd test1
+
+touch file1.txt file2.txt 
+
+echo "DevOps is considered a set of practices that combines the abilities of Software Development i.e Dev and IT Operations i.e Ops together, which results in delivering top-notch quality software fastly and more efficiently. Its focus is to encourage communication, collaboration, and integration between the software development team and IT operations to maintain the software development lifecycle." >> file1.txt
+
+
