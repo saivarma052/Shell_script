1237f67e288a942498a353799a8a1cb306f263df
saivarma052 <saivarmadanduprlou052@gmail.com>
2024-12-10 11:05:23 +0530

    updating script for mysql

--- 09-installmysql.sh
+++ 09-installmysql.sh
@@ -10,6 +10,9 @@ else
     echo "you are root user"
 fi
 
+yum install python -y
+
 sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022wget http://dev.mysql.com/get/mysql57-community-release-el7-8.noarch.rpm
 sudo yum localinstall -y mysql57-community-release-el7-8.noarch.rpm
 sudo yum install -y mysql-community-server
+
