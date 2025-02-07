9b0a68d36922394edf60b1624a2ae39b7f26ffc0
saivarma052 <saivarmadanduprlou052@gmail.com>
2025-02-07 18:39:56 +0530

    adding apt

---
+++ apt.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+ 
+ID=$(id -u)
+USER_NAME=$(whoami)
+ 
+if [ "$ID" -ne 0 ]; then
+    if [ "$USER_NAME" == "ec2-user" ]; then
+        echo "ERROR:: You are ec2-user and need root access. Switching to root user..."
+        su -c "bash $0"
+        exit $? # Exit after the script is re-run as root
+    else
+        echo "ERROR:: Please run this script with root access"
+        exit 1
+    fi
+else
+    echo "You are the root user. Proceeding with installations..."
+fi
+ 
+# Install git
+apt install git -y
+if [ "$?" -ne 0 ]; then
+    echo "ERROR:: Git installation failed"
+    exit 1
+else
+    echo "SUCCESS:: Git installation completed"
+fi
+ 
+# Install python
+apt install python -y
+if [ "$?" -ne 0 ]; then
+    echo "ERROR:: Python installation failed"
+    exit 1
+else
+    echo "SUCCESS:: Python installation completed"
+fi
+
+#Install nginx
+apt install nginx -y
+if ["$?" -ne 0 ]; then
+    echo "ERROR:: nginx installation failed"
+    exit 1
+else
+    echo "SUCCESS:: nginx installation completed"
+fi
+
+groupadd linuxadmin
+groupadd devops
+groupadd dev
+groupadd infra
+
+if ["$?" -ne 0 ]; then
+    echo "ERROR:: groups not added"
+    exit 1
+else
+    echo "SUCCESS:: Groups added successfully"
+fi
+
+adduser john 
+adduser max
+adduser sam
+adduser ross
+
+if ["$?" -ne 0 ]; then
+    echo "ERROR:: unable to add users"
+    exit 1
+else
+    echo "SUCCESS:: users added successfully"
+fi
+
+usermod -aG linuxadmin john
+usermod -aG devops john
+usermod -aG dev john
+usermod -aG infra john
+
+if ["$?" -ne 0 ]; then
+    echo "ERROR:: unble to add john as admin for groups"
+    exit 1
+else
+    echo "SUCCESS:: Successfully added john as groups admin"
+fi
+
+gpasswd -A john linuxadmin
+gpasswd -A john devops
+gpasswd -A john dev
+gpasswd -A john infra
+
+vim /etc/nginx/nginx.conf
+
