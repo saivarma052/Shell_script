f50395cefdf0490a117da7131c6d10d1deac5ec1
saivarma052 <saivarmadanduprlou052@gmail.com>
2025-02-09 23:47:44 +0530

    adding passwd

---
+++ passwd.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+ 
+ID=$(id -u)
+USER_NAME=$(whoami)
+ 
+if [ "$ID" -ne 0 ]; then
+    if [ "$USER_NAME" == "ec2-user" ]; then
+        echo "ERROR:: You are ec2-user and need root access. Switching to root user..."
+        su -c "bash $0"
+        exit $? # Exit after the script is re-run as root
+    else
+        echo "ERROR:: Please run this script with root access"
+        exit 1
+    fi
+else
+    echo "You are the root user. Proceeding with installations..."
+fi
+
+
+adduser saidandu
+
+groupadd devops
+
+gpasswd -a saidandu devops
+
+passwd -e saidandu
+
+
+
+
